3d8ee5b3756204e1a14e72d2ae89b0195ba23101
theakash04 <aakashkkr04@gmail.com>
2024-04-03 21:03:11 +0530

    test 6

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import CommingSoon from "../../components/CommingSoon";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  function start(){
+    navigate("/start")
+  }
   return (
     <>
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-screen flex items-center justify-center flex-col gap-20">
       <CommingSoon />
+      <Button onClick={start} variant="contained" size="medium" >see Start page</Button>
     </div>
     </>
   );
