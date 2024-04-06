bc5efff95eef644288c07cc3e5c5183f80f7f0ae
theakash04 <aakashkkr04@gmail.com>
2024-04-06 23:47:58 +0530

    Error page added
    made some minor style change

--- src/Pages/Error/ErrorPage.jsx
+++ src/Pages/Error/ErrorPage.jsx
@@ -12,10 +12,11 @@ function ErrorPage() {
             <img src={error} alt="Error" />
           </div>
 
-          <p className='text-3xl font-extrabold'>Opps!</p>
+          <p className='text-5xl font-extrabold'>Opps!</p>
           
-          <span className='py-5 text-center'>
-            We’re sorry. The page you requested could not be found.
+          <span className='py-7 text-center font-semibold text-white/70'>
+            <p>We’re sorry.</p>
+            <p>The page you requested could not be found.</p>
           </span>
 
           <div className='flex items-center justify-center w-full gap-5 sm:flex-row flex-col'>
