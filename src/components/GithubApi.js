7aae41378b2945e92fc7309023f0b9c9503da669
theakash04 <aakashkkr04@gmail.com>
2024-04-19 13:17:08 +0530

    gihub api added

--- src/components/GithubApi.js
+++ src/components/GithubApi.js
@@ -1,26 +1,6 @@
-// calling github api here and sending data from here to other components if needed
-
-const GitApi = "https://api.github.com/users/theakash04"
-let imageUrl;
-
-async function CallApi(){
-  try {
-    const fetched = await fetch(GitApi);
-    const JsonData = await fetched.json()
-    imageUrl = await JsonData.avatar_url;
-  } catch (error) {
-    console.log("ERROR :: Caling Git Api ", error)
-  }
-
-}
-CallApi();
-
-
-export default async function getImageUrl(){
-  await CallApi();
-  console.log(imageUrl)
-  return imageUrl;
+export const githubInfoLoader = async() =>{
+  const res = await fetch('https://api.github.com/users/theakash04')
+  return res.json();
 }
 
-getImageUrl()
-
+githubInfoLoader();
