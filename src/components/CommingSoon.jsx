3701658431cd6aefdba99f64fc2681abbfda09c1
theakash04 <aakashkkr04@gmail.com>
2024-05-19 00:24:30 +0530

    Added link to start page

--- src/components/CommingSoon.jsx
+++ src/components/CommingSoon.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ButtonStyled from "./HeroSection/ButtonStyled";
 import { Link } from "react-router-dom";
 
 function CommingSoon() {
@@ -14,9 +13,16 @@ function CommingSoon() {
         </span>
       </h1>
 
-      <div>
+      <div className="flex gap-5 items-center">
         <Link to={"/"}>
-          <ButtonStyled children={"Go to HomePage"}/>
+          <button className="bg-[#000] text-[#ffff] cursor-pointer font-semibold px-4 rounded-lg hover:bg-[#000]/90 transition-all ease-in-out py-3 shadow-md">
+            Home Page
+          </button>
+        </Link>
+        <Link to={"/start"}>
+          <button className="bg-[#DAE2ED] text-[#434D5B] cursor-pointer font-semibold px-4 rounded-lg hover:bg-[#DAE2ED]/90 transition-all ease-in-out py-3 shadow-md">
+            Start Page
+          </button>
         </Link>
       </div>
     </div>
