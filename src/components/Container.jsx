9011b6958058e980b31e590d7fd4b6a15a29e08b
theakash04 <aakashkkr04@gmail.com>
2024-05-25 18:43:19 +0530

    Edit in Containercomponent

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children, className, ...props}) {
   return (
-    <div className={`w-screen h-full bg-[#1c1e21] text-white backdrop-blur-md ${className}`} {...props}>
+    <div className={`w-screen h-full bg-[#1c1e21] text-white backdrop-blur-md overflow-x-hidden ${className}`} {...props}>
       {children}
     </div>
   )
