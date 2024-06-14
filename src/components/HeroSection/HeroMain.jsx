eba0f759fead04ba13862a61d4e3b83ec9b7c9b6
Akash kumar <Aakashkkr04@gmail.com>
2024-06-14 23:20:57 +0530

    Remove unused SVG files & added a animation on homepage

--- src/components/HeroSection/HeroMain.jsx
+++ src/components/HeroSection/HeroMain.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import TextLogo from "./TextLogo";
 import GsapBtn from "./GsapBtn";
 
 function HeroMain() {
   return (
-    <div className="w-screen px-5 pt-5 flex flex-col pb-5 relative" id="hero-main-section">
-      <div className="flex justify-center mb-10">
-        <span className="bg-[#b2dffe] flex items-center px-3 py-1 rounded-md text-[#106ba7] font-semibold gap-2 justify-center">
-          <span className="bg-green-600 w-3 h-3 rounded-full animate-pulse " />
-          <p>Available for work</p>
-        </span>
-      </div>
+    <div
+      className="w-screen px-5 pt-5 flex flex-col pb-5 relative"
+      id="hero-main-section"
+    >
       <div className="w-full flex flex-col items-center py-5">
         <div className="flex flex-col items-center gap-5">
           {/* intro */}
-          <p className="text-5xl font-bold text-center w-full">Hello! I am Akash</p>
+
+          <p className="text-5xl font-bold text-center w-full">
+            <span className="font-extrabold hover:text-green-500 transition-all">Hello!</span> I am Akash
+          </p>
           {/* profession */}
-          <p className="font-semibold text-lg text-[#c6c6c6]">WEB DEVELOPER</p>
+          <div className="flex justify-center items-center gap-4">
+            <span className="tooltip-container">
+              <span className="tooltip">Available for Work</span>
+              <span className="text"></span>
+            </span>
+            <p className="font-semibold text-lg text-[#c6c6c6]">
+              WEB DEVELOPER
+            </p>
+          </div>
         </div>
-        <div className="text-center pt-10 sm:px-16 text-2xl font-bold text-[#b6b6b6]">
+        <div className="text-center pt-10 text-2xl font-bold text-[#b6b6b6] relative">
           Until you do something on your own in programming, nothing will happen
+          <div className="run" />
         </div>
 
         <div className="flex gap-5 mt-10 items-center justify-center">
