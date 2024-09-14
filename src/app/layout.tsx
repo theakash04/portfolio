14793cafcfc101ac02a3824d921c6585af8813b9
Akash kumar <Aakashkkr04@gmail.com>
2024-09-14 20:34:29 +0530

    [Added]: change text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description:
     "Explore the portfolio of Akash, a skilled Full-Stack Developer with expertise in modern web technologies, building responsive and user-friendly applications. Discover projects, skills, and experience in creating efficient and scalable solutions.",
   keywords:
-    "Akash, Full-Stack Developer, Web Development, Portfolio, JavaScript, React, Node.js, Next.js, CSS, Tailwind, Responsive Design",
+    "Akash, Full-Stack Developer, Web Development, Portfolio, JavaScript, React, Node.js, Next.js, CSS, Tailwind, Responsive Design, theakash04, @theakash04",
   openGraph: {
     title: "Akash - Full-Stack Developer Portfolio",
     description:
